b7212284901538a1e82b321f0831095e4a6394b7
Phoenix <117663444@qq.com>
2019-11-26 22:24:10 +0800

    准备演示表格

--- src/app/pages/yanglao/yanglao.component.ts
+++ src/app/pages/yanglao/yanglao.component.ts
@@ -3,13 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-yanglao',
   templateUrl: './yanglao.component.html',
-  styleUrls: ['./yanglao.component.css']
+  styleUrls: ['./yanglao.component.css'],
 })
 export class YanglaoComponent implements OnInit {
+  listOfData = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/pages/yanglao/yanglao.module.ts
+++ src/app/pages/yanglao/yanglao.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { YanglaoComponent } from './yanglao.component';
 import { YanglaoRoutingModule } from './yanglao-routing.module';
+import { NzTableModule, NzDividerModule } from 'ng-zorro-antd';
 
 @NgModule({
   declarations: [YanglaoComponent],
-  imports: [CommonModule, YanglaoRoutingModule],
+  imports: [CommonModule, YanglaoRoutingModule, NzTableModule, NzDividerModule],
 })
 export class YanglaoModule {}
