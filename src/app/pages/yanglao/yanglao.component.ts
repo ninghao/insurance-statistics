7fd4ddfcf49b46928d13101238ef51a35b721d2f
Phoenix <117663444@qq.com>
2019-11-27 08:51:56 +0800

    过滤与搜索表格数据

--- src/app/pages/yanglao/yanglao.component.ts
+++ src/app/pages/yanglao/yanglao.component.ts
@@ -7,7 +7,58 @@ import * as Papa from 'papaparse';
   styleUrls: ['./yanglao.component.css'],
 })
 export class YanglaoComponent implements OnInit {
+  searchValue = '';
+  sortName: string | null = null;
+  sortValue: string | null = null;
+  listOfFilterAddress = [];
+  listOfSearchAddress: string[] = [];
+
   listOfData = [];
+  listOfDisplayData = [...this.listOfData];
+
+  reset() {
+    this.searchValue = '';
+    this.search();
+  }
+
+  sort(sortName: string, value: string) {
+    this.sortName = sortName;
+    this.sortValue = value;
+    this.search();
+  }
+
+  filterAddressChange(value: string[]) {
+    this.listOfSearchAddress = value;
+    this.search();
+  }
+
+  search() {
+    console.log('search');
+
+    const filterFunc = item => {
+      return (
+        (this.listOfSearchAddress.length
+          ? this.listOfSearchAddress.some(
+              address => item.街道乡镇.indexOf(address) !== -1,
+            )
+          : true) && item.社区村组.indexOf(this.searchValue) !== -1
+      );
+    };
+
+    const data = this.listOfData.filter(item => filterFunc(item));
+
+    this.listOfDisplayData = data;
+
+    // this.listOfDisplayData = data.sort((a, b) =>
+    //   this.sortValue === 'ascend'
+    //     ? a[this.sortName!] > b[this.sortName!]
+    //       ? 1
+    //       : -1
+    //     : b[this.sortName!] > a[this.sortName!]
+    //     ? 1
+    //     : -1,
+    // );
+  }
 
   constructor() {}
 
@@ -18,7 +69,19 @@ export class YanglaoComponent implements OnInit {
       skipEmptyLines: true,
       complete: (result, file) => {
         console.log(result);
+        // 设置表格数据
         this.listOfData = result.data;
+
+        // 街道选项列表
+        const streets = result.data.map(item => item.街道乡镇);
+
+        this.listOfFilterAddress = [...new Set(streets)].map(item => {
+          return {
+            text: item,
+            value: item,
+          };
+        });
+        this.listOfDisplayData = [...this.listOfData];
       },
     });
   }
--- src/app/pages/yanglao/yanglao.module.ts
+++ src/app/pages/yanglao/yanglao.module.ts
@@ -2,10 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { YanglaoComponent } from './yanglao.component';
 import { YanglaoRoutingModule } from './yanglao-routing.module';
-import { NzTableModule, NzDividerModule } from 'ng-zorro-antd';
+import {
+  NzTableModule,
+  NzDividerModule,
+  NzDropDownModule,
+  NzInputModule,
+  NZ_ICONS,
+  NzButtonModule,
+  NzIconModule,
+} from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [YanglaoComponent],
-  imports: [CommonModule, YanglaoRoutingModule, NzTableModule, NzDividerModule],
+  imports: [
+    CommonModule,
+    YanglaoRoutingModule,
+    NzTableModule,
+    NzDividerModule,
+    NzDropDownModule,
+    NzInputModule,
+    NzButtonModule,
+    NzIconModule,
+    FormsModule,
+  ],
 })
 export class YanglaoModule {}
